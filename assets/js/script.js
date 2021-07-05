9ace266791f2dcbb65d0c930745c7f6af6a21197
David Berkheimer <berkheimer112@atlanticbb.net>
2021-07-05 11:32:53 -0400

    added static method to build all Keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,86 +15,9 @@ window.onload = function () {
 };
 //Start Up
 function startUp() {
-  currentKey = createKeys();
+  currentKey = Key.buildCircularLinkedListOfKeys();
   display.paintKeyDiv(currentKey.getKeyHtml());
   display.paintChordProgressionDiv(currentKey.getChordProgressionsHtml());
-}
-
-function createKeys() {
-  let C = new Key(["C", "D", "E", "F", "G", "A", "B"], 0, 0);
-  let G = new Key(["G", "A", "B", "C", "D", "E", "F#"], 1, 0);
-  let D = new Key(["D", "E", "F#", "G", "A", "B", "C#"], 2, 0);
-  let A = new Key(["A", "B", "C#", "D", "E", "F#", "G#"], 3, 0);
-  let EFflat = new Key(["E", "F#", "G#", "A", "B", "C#", "D#"], 4, 0);
-  let BCflat = new Key(["B", "C#", "D#", "E", "F#", "G#", "A#"], 5, 0);
-  let FsharpGflat = new Key(["F#", "G#", "A#", "B", "C#", "D#", "E#"], 6, 0);
-  //cutoff point
-  let CsharpDflat = new Key(["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"], 0, 5);
-  let AflatGsharp = new Key(["Ab", "Bb", "C", "Db", "Eb", "F", "G"], 0, 4);
-  let Eflat = new Key(["Eb", "F", "G", "Ab", "Bb", "C", "D"], 0, 3);
-  let Bflat = new Key(["Bb", "C", "D", "Eb", "F", "G", "A"], 0, 2);
-  let F = new Key(["F", "G", "A", "Bb", "C", "D", "E"], 0, 1);
-
-  // Set next and prev keys, this is a linked list
-  C.setPrevKey(F);
-  C.setNextKey(G);
-
-  G.setPrevKey(C);
-  G.setNextKey(D);
-
-  D.setPrevKey(G);
-  D.setNextKey(A);
-
-  A.setPrevKey(D);
-  A.setNextKey(EFflat);
-
-  EFflat.setPrevKey(A);
-  EFflat.setNextKey(BCflat);
-
-  BCflat.setPrevKey(EFflat);
-  BCflat.setNextKey(FsharpGflat);
-
-  FsharpGflat.setPrevKey(BCflat);
-  FsharpGflat.setNextKey(CsharpDflat);
-
-  CsharpDflat.setPrevKey(FsharpGflat);
-  CsharpDflat.setNextKey(AflatGsharp);
-
-  AflatGsharp.setPrevKey(CsharpDflat);
-  AflatGsharp.setNextKey(Eflat);
-
-  Eflat.setPrevKey(AflatGsharp);
-  Eflat.setNextKey(Bflat);
-
-  Bflat.setPrevKey(Eflat);
-  Bflat.setNextKey(F);
-
-  F.setPrevKey(Bflat);
-  F.setNextKey(C);
-
-  // create enharmonic Keys and set them to correct keys
-  let CsharpDflatEnharmonic = new Key(
-    ["C#", "D#", "E#", "F#", "G#", "A#", "B#"],
-    7,
-    0
-  );
-  CsharpDflat.setEnharmonicKey(CsharpDflatEnharmonic);
-
-  let FsharpGflatEnharmonic = new Key(
-    ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
-    0,
-    6
-  );
-  FsharpGflat.setEnharmonicKey(FsharpGflatEnharmonic);
-
-  let BCflatEnharmonic = new Key(
-    ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"],
-    0,
-    7
-  );
-  BCflat.setEnharmonicKey(BCflatEnharmonic);
-
-  return C;
 }
 
 el.prevKeyBtn.addEventListener("click", (e) => {
